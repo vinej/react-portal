5960c06e51e16307035e6c5edc63526bc2bb0ab2
Jean-Yves Vinet <jyvinet@hotmail.ca>
2016-05-21 21:13:54 -0400

    Update message_types.js

--- src/types/message_types.js
+++ src/types/message_types.js
@@ -1,2 +1,2 @@
-export const MESSAGE_SET = 'message_set';
-export const MESSAGE_ERROR = 'message_error';
+export const MESSAGE_SET    = 'message_set';
+export const MESSAGE_ERROR  = 'message_error';
