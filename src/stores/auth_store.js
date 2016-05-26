8addb54ba0e87e4e8d2d660f416e7a16dad9d7c0
vinej <jyvinet@hotmail.ca>
2016-05-26 19:05:00 -0400

    Mobx 2.2.1 with @action decorator

--- src/stores/auth_store.js
+++ src/stores/auth_store.js
@@ -1,5 +1,4 @@
-import { observable } from 'mobx';
-import { transaction } from 'mobx';
+import { observable, action } from 'mobx';
 
 export var authStore = {
   @observable email : "",
@@ -24,68 +23,65 @@ export var authFormStore = {
 
   isError : false,
 
+  @action
   reset : function() {
-    transaction( () => {
-      this.email = "";
-      this.name = "";
-      this.password = "";
-      this.passwordConfirm = "";
-      this.emailError = '';
-      this.nameError = '';
-      this.passwordError = '';
-      this.passwordConfirmError = '';
-    });
+    this.email = "";
+    this.name = "";
+    this.password = "";
+    this.passwordConfirm = "";
+    this.emailError = '';
+    this.nameError = '';
+    this.passwordError = '';
+    this.passwordConfirmError = '';
   },
 
+  @action
   isValidateSignUp : function() {
-    transaction( () => {
-      this.isError = false;
-      this.emailError = '';
-      this.nameError = '';
-      this.passwordError = '';
-      this.passwordConfirmError = '';
+    this.isError = false;
+    this.emailError = '';
+    this.nameError = '';
+    this.passwordError = '';
+    this.passwordConfirmError = '';
 
-      if (!this.email) {
-        this.emailError = "Enter a email address";
-        this.isError = true;
-      }
+    if (!this.email) {
+      this.emailError = "Enter a email address";
+      this.isError = true;
+    }
 
-      if (!this.password) {
-        this.passwordError = "Enter a password";
-        this.isError = true;
-      }
+    if (!this.password) {
+      this.passwordError = "Enter a password";
+      this.isError = true;
+    }
 
-      if (this.password != this.passwordConfirm) {
-        this.passwordConfirmError = "Both password must be equal";
-        this.isError = true;
-      }
+    if (this.password != this.passwordConfirm) {
+      this.passwordConfirmError = "Both password must be equal";
+      this.isError = true;
+    }
 
-      if (!this.name) {
-        this.nameError = "Enter a name";
-        this.isError = true;
-      }
-    });
+    if (!this.name) {
+      this.nameError = "Enter a name";
+      this.isError = true;
+    }
 
     return this.isError === false;
   },
 
+  @action
   isValidateSignIn : function() {
-    transaction( () => {
-      this.isError = false;
-      this.emailError = '';
-      this.passwordError = '';
+    this.isError = false;
+    this.emailError = '';
+    this.passwordError = '';
 
-      if (!this.email) {
-        this.emailError = "Enter a email address";
-        this.isError = true;
-      }
+    if (!this.email) {
+      this.emailError = "Enter a email address";
+      this.isError = true;
+    }
 
-      if (!this.password) {
-        this.passwordError = "Enter a password";
-        this.isError = true;
-      }
+    if (!this.password) {
+      this.passwordError = "Enter a password";
+      this.isError = true;
+    }
 
-    });
     return this.isError === false;
   }
 }
