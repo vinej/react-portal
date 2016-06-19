d2894fedbd570e155f07e0d38a5692a0f67061aa
vinej <jyvinet@hotmail.ca>
2016-06-18 23:56:32 -0400

    React.PropTypes validation and model

--- src/stores/auth_store.js
+++ src/stores/auth_store.js
@@ -77,6 +77,7 @@ export default class AuthStore {
     dispatch(tabbarCloseAll())
   }
 
+  authError(error, mainComponentsToRender) {
     transaction(() => {
       if (mainComponentsToRender) {
         authStore.errorMessage = ''
